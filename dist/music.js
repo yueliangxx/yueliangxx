9023e4b5a1a4f2502b120f9169455ef3ea23fa3d
yueliangxx <15999924008@163.com>
2021-04-22 20:38:53 +0800

    Site updated: 2021-04-22 20:38:52

--- dist/music.js
+++ dist/music.js
@@ -1,10 +1,14 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
-    mini: true,
-    audio: [{
-        name: '红色高跟鞋',
-        artist: '杜航',
-        url: 'https://music.163.com/#/song?id=864043623',
-        cover: 'http://luna.yueliangxx.com/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20210422181441.jpg',
-    }]
+    container: document.getElementById('aplayer'),
+    fixed: true,   #吸底模式
+    autoplay: false,  #自动播放
+    audio: [
+	 {
+		name: '红色高跟鞋',
+		artist: '红色高跟鞋',
+		url: 'http://music.163.com/song/media/outer/url?id=864043623.mp3',
+		cover: 'http://p2.music.126.net/IZ5hcrme7k-xNG4gzw-NPg==/109951164627195322.jpg??',
+	    lrc: 'images/.lrc' 
+	 },
+    ]
 });
