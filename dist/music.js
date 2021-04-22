b05eb2a6bab9b4cc45681b9738a13903a6d46fee
yueliangxx <15999924008@163.com>
2021-04-22 18:34:01 +0800

    Site updated: 2021-04-22 18:33:59

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    mini: true,
+    audio: [{
+        name: '红色高跟鞋',
+        artist: '杜航',
+        url: 'https://music.163.com/#/song?id=864043623',
+        cover: 'http://luna.yueliangxx.com/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20210422181441.jpg',
+    }]
+});
